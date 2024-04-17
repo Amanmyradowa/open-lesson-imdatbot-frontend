2c02b4bcd1f58687171ea9ca6695f8c315de234d
Imdat Developers <imdatdevelopers@Imdats-iMac.local>
2024-04-17 17:53:40 +0500

    swiper

---
+++ assets/js/index.js
@@ -0,0 +1,16 @@
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  // loop: true,
+
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false, 
+  // },
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  speed: 1500,
+
+});
