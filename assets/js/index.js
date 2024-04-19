09eda77ba2259adbeb9ee577787e4d31b54bdbf8
Imdat Developers <imdatdevelopers@Imdats-iMac.local>
2024-04-19 17:24:32 +0500

    mobile responsive nad refactoring background shape

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,34 +22,33 @@ const swiper = new Swiper('.swiper', {
 
 });
 
-// const swiper1 = new Swiper('.swiper1', {
-//   slidesPerView: 2,
-//   // loop: true,
+const swiper1 = new Swiper('.swiper1', {
+  slidesPerView: 2,
+  // loop: true,
 
-//   // autoplay: {
-//   //   delay: 2000,
-//   //   disableOnInteraction: false, 
-//   // },
+  // autoplay: {
+  //   delay: 2000,
+  //   disableOnInteraction: false, 
+  // },
 
-//   pagination: {
-//     el: '.swiper-pagination1',
-//   },
+  pagination: {
+    el: '.swiper-pagination1',
+  },
 
-//   breakpoints: {
-//     840: {
-//     slidesPerView: 1,
-//     slideToClickedSlide: true,
-//   }
-//   },
+  breakpoints: {
+    840: {
+    slidesPerView: 1,
+    slideToClickedSlide: true,
+  }
+  },
 
-//   speed: 1500,
+  speed: 1500,
 
-// });
+});
 
 
 let width = screen.width;
 
-console.log(width);
 
 let swiperWrapper = document.querySelector('.swiper-wrapper');
 let swiperPagination = document.querySelector('.swiper-pagination');
@@ -61,10 +60,10 @@ if(width > 840)
 }
 
 // let swiperWrapper1 = document.querySelector('.swiper-wrapper1');
-// let swiperPagination1 = document.querySelector('.swiper-pagination1');
+let swiperPagination1 = document.querySelector('.swiper-pagination1');
 
-// if(width > 840)
-// {
-//   swiperWrapper1.classList.add('disabled');
-//   swiperPagination1.classList.add('disabled');
-// }
+if(width > 840)
+{
+  swiperWrapper.classList.add('disabled');
+  swiperPagination1.classList.add('disabled');
+}
