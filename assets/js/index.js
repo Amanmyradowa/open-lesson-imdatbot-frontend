0bbb4126207e165e9cd9088e1c612bbfe4bc4771
Imdat Developers <imdatdevelopers@Imdats-iMac.local>
2024-04-24 17:30:28 +0500

    refactoring

--- assets/js/index.js
+++ assets/js/index.js
@@ -11,6 +11,11 @@ const swiper = new Swiper('.swiper', {
 
     960: {
       slidesPerView: 6,
+      spaceBetween: 0
+    },
+
+    840: {
+      slidesPerView: 3,
       spaceBetween: 30
     },
 
@@ -36,7 +41,7 @@ const swiper = new Swiper('.swiper', {
 });
 
 const swiper1 = new Swiper('.swiper1', {
-  slidesPerView: 1,
+  slidesPerView: 3,
   spaceBetween: 0,
   loop: true,
   rewind: true,
@@ -48,6 +53,53 @@ const swiper1 = new Swiper('.swiper1', {
   breakpoints: {
 
     960: {
+      slidesPerView: 3,
+      spaceBetween: 0
+    },
+
+    840: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    720: {
+      slidesPerView: 2,
+      spaceBetween: 30
+    },
+
+    540: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 30
+    },
+
+  },
+
+  speed: 1500,
+
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  slidesPerView: 2,
+  spaceBetween: 0,
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination2',
+  },
+
+  breakpoints: {
+
+    960: {
+      slidesPerView: 6,
+      spaceBetween: 0
+    },
+
+    840: {
       slidesPerView: 3,
       spaceBetween: 30
     },
@@ -74,32 +126,13 @@ const swiper1 = new Swiper('.swiper1', {
 });
 
 
-const swiper2 = new Swiper('.swiper2', {
-  slidesPerView: 2,
-  spaceBetween: 30,
-
-  pagination: {
-    el: '.swiper-pagination2',
-  },
-
-  breakpoints: {
-    840: {
-    slidesPerView: 2,
-    slideToClickedSlide: true,
-  }
-  },
-
-  speed: 1500,
-
-});
-
-
 let width = screen.width;
-
 
 let swiperWrapper = document.querySelectorAll('.swiper-wrapper');
 let swiperPagination = document.querySelector('.swiper-pagination');
-let swiperSlide = document.querySelectorAll('.swiper-slide');
+let swiperSlide = document.querySelectorAll('.swiper .swiper-slide');
+let swiperSlide1 = document.querySelectorAll('.swiper1 .swiper-slide');
+let swiperSlide2 = document.querySelectorAll('.swiper2 .swiper-slide');
 let swiperPagination1 = document.querySelector('.swiper-pagination1');
 let swiperPagination2 = document.querySelector('.swiper-pagination2');
 
@@ -108,12 +141,23 @@ if(width > 960)
 {
   swiperPagination.classList.add('disabled');
   swiperPagination1.classList.add('disabled');
+  swiperPagination2.classList.add('disabled');
   swiperWrapper[0].classList.add('disabled');
   swiperWrapper[1].classList.add('disabled');
   swiperWrapper[2].classList.add('disabled');
+  
   for(let i=0; i<swiperSlide.length; i++)
   {
     swiperSlide[i].classList.add('grid-image-description__item-size-30');
+  }
+  for(let i=0; i<swiperSlide1.length; i++)
+  {
+    swiperSlide1[i].classList.add('grid-image-description__item-size-30');
+
+  }
+  for(let i=0; i<swiperSlide2.length; i++)
+  {
+    swiperSlide2[i].classList.add('grid-image-description__item-size-22');
   }
 } else
 {
@@ -121,15 +165,13 @@ if(width > 960)
   {
     swiperSlide[i].classList.remove('grid-image-description__item-size-30');
   }
+  for(let i=0; i<swiperSlide1.length; i++)
+  {
+    swiperSlide1[i].classList.remove('grid-image-description__item-size-30');
+  }
+  for(let i=0; i<swiperSlide2.length; i++)
+  {
+    swiperSlide2[i].classList.remove('grid-image-description__item-size-22');
+  }
 
 }
-
-
-
-// let swiperPagination2 = document.querySelector('.swiper-pagination1');
-
-// if(width > 840)
-// {
-//   swiperWrapper.classList.add('disabled');
-//   swiperPagination2.classList.add('disabled');
-// }
