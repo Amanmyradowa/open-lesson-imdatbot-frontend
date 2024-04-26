a157053f93978efd3efc16a750f39ca9056510b3
Imdat Developers <imdatdevelopers@Imdats-iMac.local>
2024-04-26 17:38:28 +0500

    refactoring responsive

--- assets/js/index.js
+++ assets/js/index.js
@@ -175,3 +175,72 @@ if(width > 960)
   }
 
 }
+
+
+// class ResponsiveGrid
+// {
+
+//   element;
+//   wrapperSelector;
+//   paginationSelector;
+//   slideClass;
+//   sizeClassLarge;
+//   sizeClassSmall;
+
+//   constructor(selectors = "")
+//   {
+//     this.element = document.querySelector(selectors);
+//     this.wrapperSelector = this.element.querySelector();
+//     this.paginationSelector = this.element.querySelector();
+//   }
+// }
+
+// class ResponsiveGrid {
+//   constructor(wrapperSelector, paginationSelector, slideClass, sizeClassLarge, sizeClassSmall) {
+//     this.wrapper = document.querySelector(wrapperSelector);
+//     this.pagination = document.querySelector(paginationSelector);
+//     this.slides = document.querySelectorAll(slideClass);
+//     this.sizeClassLarge = sizeClassLarge;
+//     this.sizeClassSmall = sizeClassSmall;
+//     this.breakpoint = 960;
+//   }
+
+//   resizeHandler() {
+//     const width = screen.width;
+//     if (width > this.breakpoint) {
+//       this.disablePagination();
+//       this.resizeSlides(this.sizeClassLarge);
+//     } else {
+//       this.enablePagination();
+//       this.resizeSlides(this.sizeClassSmall);
+//     }
+//   }
+
+//   disablePagination() {
+//     this.pagination.classList.add('disabled');
+//     this.wrapper.classList.add('disabled');
+//   }
+
+//   enablePagination() {
+//     this.pagination.classList.remove('disabled');
+//     this.wrapper.classList.remove('disabled');
+//   }
+
+//   resizeSlides(sizeClass) {
+//     for (const slide of this.slides) {
+//       slide.classList.remove(this.sizeClassLarge, this.sizeClassSmall);
+//       slide.classList.add(sizeClass);
+//     }
+//   }
+// }
+
+// const grid1 = new ResponsiveGrid('.swiper-wrapper', '.swiper-pagination', '.swiper-slide','grid-image-description__item-size-30', 'grid-image-description__item-size-22');
+
+// const grid2 = new ResponsiveGrid('.swiper1 .swiper-wrapper', '.swiper-pagination1', '.swiper1 .swiper-slide','grid-image-description__item-size-30', 'grid-image-description__item-size-22');
+// // ... создайте больше экземпляров для других сеток при необходимости
+
+// window.addEventListener('resize', () => {
+//   grid1.resizeHandler();
+//   grid2.resizeHandler();
+//   // ... вызовите resizeHandler для других сеток
+// });
