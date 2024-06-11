6abbfe5d8db3e757c96566d7e8dba4936a6643eb
hydyrayhan <hydyrowayhan7@gmail.com>
2024-06-12 00:36:03 +0500

    refactoring

--- assets/js/LazyLoad.js
+++ assets/js/LazyLoad.js
@@ -35,29 +35,32 @@
 function createPreloder() {
   const preloaderElement = document.createElement('div');
   preloaderElement.setAttribute('class','preloader')
-  preloaderElement.innerHTML =  
-    `<svg class="icon-size-preloader">
-      <use xlink:href="./assets/icons/sprite.svg#loading"></use>
-    </svg>`
+  preloaderElement.innerHTML =  'loading';
+    // `<svg class="icon-size-preloader">
+    //   <use xlink:href="./assets/icons/sprite.svg#loading"></use>
+    // </svg>`
 
     return preloaderElement;
 }
 
 let lazyImages = document.querySelectorAll('.lazy');
+lazyImages.forEach(image => {
+  image.parentElement.appendChild(createPreloder());
+  image.setAttribute('loading', 'lazy');
+})
 
 let lazyImageObserver = new IntersectionObserver(entries => {
   entries.forEach(entry => {
     let img = entry.target;
     let loaderContainer = img.parentElement;
     
-    img.parentElement.appendChild(createPreloder());
-    let preloaderElement = loaderContainer.querySelector('.preloader');
-    img.setAttribute('loading', 'lazy');
+    
     
     if(entry.isIntersecting) {
       img.src = img.dataset.src;
       
       img.addEventListener('load', function() {
+        let preloaderElement = this.parentElement.querySelector('.preloader');
         loaderContainer.classList.remove('loading-theme__white');
         loaderContainer.classList.remove('loading-theme__grey');
         loaderContainer.removeChild(preloaderElement);
